24d6d4611fc451a079a8106f4da869733f29541e
Rasmus Fuhse <fuhse@data-quest.de>
2018-12-12 16:26:25 +0100

    hide empty results

--- views/freetext/freetext_evaluation.php
+++ views/freetext/freetext_evaluation.php
@@ -8,6 +8,7 @@ $etask = $vote->etask;
 
 <ul class="clean">
     <? foreach ($vote->answers as $answer) : ?>
+        <? if (trim($answer['answerdata']['text'])) : ?>
         <li style="border: #d0d7e3 thin solid; margin: 10px; padding: 10px;">
             <? if (!$vote->questionnaire['anonymous']) : ?>
                 <div style="margin-bottom: 7px;">
@@ -22,6 +23,7 @@ $etask = $vote->etask;
             <? endif ?>
             <?= formatReady($answer['answerdata']['text']) ?>
         </li>
+        <? endif ?>
     <? endforeach ?>
 </ul>
 
